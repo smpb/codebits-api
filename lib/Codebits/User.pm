bfe1bdd5ac31bef550db00797f17ae1c5f77ea53
Sérgio Bernardino <me@sergiobernardino.net>
2011-09-02 01:02:00 +0100

    Updates in accordance with changes in the API.

--- lib/Codebits/User.pm
+++ lib/Codebits/User.pm
@@ -1,23 +1,27 @@
 package Codebits::User;
 
+use namespace::autoclean;
 use Moose;
 use Moose::Util::TypeConstraints;
 use Regexp::Common qw/URI/;
 
-enum 'Skill', [ qw/php perl ruby python erlang cc cocoa dotnet java javascript css api web embbeded mobile hardware microformats security sysadmin desktop design/ ];
+enum 'Skill', [ qw/api cc clojure cocoa cooking css dbdesign design desktop dotnet embbeded erlang hardware java javascript max microformats mobile nosql perl php processing python ruby scala security sysadmin visualization web/ ];
 
 # TODO - I don't yet know the designation for the rejected users
-enum 'Status', [ qw/undefined accepted review/ ];
+enum 'Status', [ qw/undefined notapplied accepted review/ ];
 
 
-has 'id' => (
+has [ 'id', 'karma' ] => (
   is  => 'ro',
   isa => subtype( 'Int' => where { $_ > 0 } ),
 );
 
-has 'blog' => (
+has [ 'blog', 'coderep' ] => (
   is  => 'rw',
-  isa => subtype( 'Str' => where { $_ eq '' or $_ =~ /$RE{URI}{HTTP}/ } ),
+  isa => subtype( 'Str' => where { $_ =~ s/https/http/i;
+      $_ eq '' or
+      $_ =~ /$RE{URI}{HTTP}/
+    } ),
 );
 
 has 'status' => (
@@ -37,10 +41,8 @@ has 'skills' => (
 
 has 'md5mail' => (
   is  => 'rw',
-  isa => subtype( 'Str' => where { $_ => /[0-9a-f]{32}/i } ),
+  isa => subtype( 'Str' => where { $_ =~ /[0-9a-f]{32}/i } ),
 );
 
 
-no Moose::Util::TypeConstraints;
-no Moose;
-1;
+42;
